c8a068df446c957debd80bd79978203acc65acbb
Kweezy <kewan3272001@gmail.com>
2022-08-16 21:58:25 +0200

    Sending directory to repository

---
+++ index.js
@@ -0,0 +1,21 @@
+const github = document.getElementById("github")
+const instagram = document.getElementById("instagram")
+const twitter = document.getElementById("twitter")
+
+const gallery = document.getElementById("gallery")
+const about = document.getElementById("about")
+const contact = document.getElementById("contact")
+
+const greetingTitle = document.getElementById("Title")
+const context = document.getElementById("context")
+
+github.append("Github")
+instagram.append("Instagram")
+twitter.append("Twitter")
+
+gallery.append("Gallery")
+about.append("About")
+contact.append("Contact")
+
+greetingTitle.append("Welcome to my HOMEPAGE!!")
+context.append("Please feel free to browse any one of my other pages on the left or visit my socials above")
