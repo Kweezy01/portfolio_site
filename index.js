4451e4acbd8903456350b1497c15347df7afac1a
Kweezy <kewan3272001@gmail.com>
2022-08-18 23:57:56 +0200

    Final files

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+const github = document.getElementById("github")
+const instagram = document.getElementById("instagram")
+const twitter = document.getElementById("twitter")
+
+const home = document.getElementById("home")
+const gallery = document.getElementById("gallery")
+const about = document.getElementById("about")
+const contact = document.getElementById("contact")
+
+const greetingTitle = document.getElementById("Title")
+const context = document.getElementById("context")
+const head_about = document.getElementById("head-about")
+const intro = document.getElementById("intro")
+const home_pic = document.getElementById("home-pic")
+
+const user_mail = document.getElementById("user-mail")
+const user_message = document.getElementById("user-message")
+const mail_button = document.getElementById("send_mail")
+
 if (window.location.href.endsWith("/home-page.html")) {
-
-   const github = document.getElementById("github")
-   const instagram = document.getElementById("instagram")
-   const twitter = document.getElementById("twitter")
-
-   const gallery = document.getElementById("gallery")
-   const about = document.getElementById("about")
-   const contact = document.getElementById("contact")
-
-   const greetingTitle = document.getElementById("Title")
-   const context = document.getElementById("context")
-   const head_about = document.getElementById("head-about")
-   const intro = document.getElementById("intro")
 
    github.append("Github")
    instagram.append("Instagram")
@@ -25,20 +31,58 @@ if (window.location.href.endsWith("/home-page.html")) {
    context.append("Please feel free to browse any one of my other pages on the left or visit my socials above")
 
    head_about.append("A little about myself")
-   intro.append("I am currently studying Python and Javascript at CTU training solutions")
-} 
+   intro.append("I am currently studying the Programming Foundation course at CTU training solutions. ")
+   intro.append("I enjoy software development and enjoy making websites, which you can see in the Gallery.")
+
+   home_pic.src = "home-pic.jpg"
+
+}
 
 else if (window.location.href.endsWith("/gallery-page.html")) {
+   about.append("About")
+   contact.append("Contact")
+   home.append("Home Page")
 
-   console.log("I am him");
+   github.append("Github")
+   instagram.append("Instagram")
+   twitter.append("Twitter")
+
 }
 
 else if (window.location.href.endsWith("/about-page.html")) {
+   gallery.append("Gallery")
+   contact.append("Contact")
+   home.append("Home Page")
 
-   console.log("I am him");
+   github.append("Github")
+   instagram.append("Instagram")
+   twitter.append("Twitter")
 }
 
 else {
 
-   console.log("I am him");
+    const mail_form = () => {  
+        const email = user_mail.value
+        const msg = user_message.value
+        var atposition=email.indexOf("@");  
+        var dotposition=email.lastIndexOf(".");  
+        if (atposition<1 || dotposition<atposition+2 || dotposition+2>=email.length || msg===""){  
+            alert("Please enter a valid e-mail address and message");
+            return false;
+        }
+        console.log(email)
+        console.log(msg)
+        alert("Thankyou for sending your message, will be in touch")
+    }
+    
+    gallery.append("Gallery")
+    about.append("About")
+    home.append("Home Page")
+    
+    github.append("Github")
+    instagram.append("Instagram")
+    twitter.append("Twitter")
+
+    mail_button.onclick = () => {mail_form()}
+    
 }
